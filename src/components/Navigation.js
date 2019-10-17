d77f6f4fd2540119db384764b55ab83ebdd76d27
Tara Sherman <taradelin@gmail.com>
2019-10-16 21:18:38 -0400

    MVP

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CartContext } from '../contexts/CartContext';
+//   - First import the `useContext` hook and our `CartContext`
 
-const Navigation = props => {
+//     - Remove `props` from our parameters
+function Navigation() {
+	//   - Next, pass our `CartContext` to the `useContext` hook and assign it to a variable named cart.
+	const cart = useContext(CartContext);
+
+	//     - Remove `props` from our cart length
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+				Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
